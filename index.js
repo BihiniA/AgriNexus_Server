aacfd41bef6c51e02d87f7531a639988619391b3
Bihini Amarasinghe <bihiniamarasinghe98@gmail.com>
2025-03-22 22:33:04 +0530

    index

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cors from "cors"; 
+import route from "./routes/plantAdminRoute.js";
 
 const app = express();
 app.use(bodyParser.json());
 dotenv.config();
 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 const PORT = process.env.PORT || 7000;
 const MONGOURL = process.env.MONGO_URL;
 
@@ -19,3 +22,5 @@ mongoose
     });
   })
   .catch((error) => console.log(error));
+
+app.use("/api", route);
